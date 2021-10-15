cdab05693dfc05d4d549b711d3e9f8fda4d5d3c5
shbzhang <shbzhang@pmo.ac.cn>
2021-10-15 09:50:19 +0200

    Update tile.py
    now tile non-celestial images

--- level3/tile.py
+++ level3/tile.py
@@ -1,33 +1,71 @@
-' mwisp module: mosaic '
+' mwisp module: tile '
 
-__version__ = '1.9'
+__version__ = '1.0'
 __author__ = 'Shaobo Zhang'
 
 from astropy.io import fits
 from astropy.wcs import WCS
 import numpy as np
 
+#modify projection frame to other galactic longitude
 def modifyframe(hdr, gl_ref=180):
-	hdr['CRVAL1'] = gl_ref
-	if hdr['CRPIX1']<0:
-		hdr['CRPIX1'] += 43200
-	hdr['CRPIX1'] -= gl_ref/0.0083333333333
+	if hdr['CTYPE1'] == 'GLON-CAR':
+		hdr['CRVAL1'] = gl_ref
+		if hdr['CRPIX1']<0:
+			hdr['CRPIX1'] += 43200
+		hdr['CRPIX1'] -= gl_ref/0.0083333333333
 	return hdr
 
-def tile(files, output='tile.fits'):
-	#find range
+#use linear convert for non-celetial axes
+def linear_WCS(hdr, axis):
+	return {key:hdr[key+axis] for key in ['NAXIS','CTYPE','CRVAL','CDELT','CRPIX','CROTA']}
+
+def linear_pixel_to_world(p, wcs, base=0):
+	return (np.array(p)-wcs['CRPIX']+1-base)*wcs['CDELT']+wcs['CRVAL']
+
+def linear_world_to_pixel(w, wcs, base=0):
+	return (np.array(w)-wcs['CRVAL'])/wcs['CDELT']+wcs['CRPIX']-1+base
+
+def tile(files, output='tile.fits', gl_ref=180):
+	'''
+	Tile images
+
+	Parameters
+	----------
+	files : list
+		a list of image name in fits format
+	output : str, optional
+		output file name. Default name is 'tile.fits'.
+	Versions
+	--------
+	Nov,09,2011,v1.0
+	'''
 	nfile = len(files)
-	mhdr = fits.getheader(files[0])
-	mhdr = modifyframe(mhdr)
-	mwcs = WCS(mhdr, naxis=2)
+	#Syntax prompt
+	if nfile < 2:
+		print("Syntax - tile(files, outputs='tile.fits')")
+		return
+	#open a fits as basic template
+	thdr = fits.getheader(files[0])
+	thdr = modifyframe(thdr, gl_ref=180)
+	try:
+		twcs = WCS(thdr, naxis=2)
+	except:
+		twcs = [linear_WCS(thdr,'1'),linear_WCS(thdr,'2')]
+	#find xy range for each file
 	xran = np.ndarray((nfile,2))
 	yran = np.ndarray((nfile,2))
-	for i,file in enumerate(files):
-		hdr = fits.getheader(file)
-		hdr = modifyframe(hdr)
-		wcs = WCS(hdr, naxis=2)
-		lb = wcs.pixel_to_world([0, hdr['NAXIS1']-1], [0, hdr['NAXIS2']-1])
-		xy = mwcs.world_to_pixel(lb)
+	for i,afile in enumerate(files):
+		hdr = fits.getheader(afile)
+		hdr = modifyframe(hdr, gl_ref=180)
+		if type(twcs) is not list:
+			wcs = WCS(hdr, naxis=2)
+			ad = wcs.pixel_to_world([0, hdr['NAXIS1']-1], [0, hdr['NAXIS2']-1])
+			xy = twcs.world_to_pixel(ad)
+		else:
+			a = linear_pixel_to_world([0, hdr['NAXIS1']-1], linear_WCS(hdr,'1'))
+			d = linear_pixel_to_world([0, hdr['NAXIS2']-1], linear_WCS(hdr,'2'))
+			xy = [linear_world_to_pixel(a, twcs[0]), linear_world_to_pixel(d, twcs[1])]
 		xran[i] = xy[0]
 		yran[i] = xy[1]
 	xran=np.round(xran).astype(int)
@@ -35,17 +73,17 @@ def tile(files, output='tile.fits'):
 	print(xran)
 
 	#header
-	#mhdr['NAXIS'] = 2
-	mhdr['NAXIS1'] = xran.max()-xran.min()+1
-	mhdr['NAXIS2'] = yran.max()-yran.min()+1
-	mhdr['CRPIX1'] -= xran.min()
-	mhdr['CRPIX2'] -= yran.min()
-	mshp = []
-	for i in range(mhdr['NAXIS'],0,-1):
-		mshp.append(mhdr['NAXIS%1i' % i])
-	mdat = np.zeros(mshp,dtype=np.float32)-1e3
+	#thdr['NAXIS'] = 2
+	thdr['NAXIS1'] = xran.max()-xran.min()+1
+	thdr['NAXIS2'] = yran.max()-yran.min()+1
+	thdr['CRPIX1'] -= xran.min()
+	thdr['CRPIX2'] -= yran.min()
 	xran -= xran.min()
 	yran -= yran.min()
+	mshp = []
+	for i in range(thdr['NAXIS'],0,-1):
+		mshp.append(thdr['NAXIS%1i' % i])
+	mdat = np.zeros(mshp,dtype=np.float32)-1e3
 	#print(mdat.shape)
 	for i,afile in enumerate(files):
 		dat = fits.open(afile)[0].data
@@ -55,11 +93,11 @@ def tile(files, output='tile.fits'):
 		#print(afile,xran[i])
 	mdat[mdat == -1e3] = np.nan
 
-	mhdu = fits.PrimaryHDU(mdat, mhdr)
+	mhdu = fits.PrimaryHDU(mdat, thdr)
 	mhdu.writeto(output, overwrite=True)
 
 
 if __name__ == '__main__':
 	import glob
-	files = glob.glob('/Users/shaobo/Work/mwips/GuideMap/*_U_m0.fits')
-	tile(files, 'tile_U_m0.fits')
+	files = glob.glob('*5_U_lvmap.fits')
+	tile(files)
