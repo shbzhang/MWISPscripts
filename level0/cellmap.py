1d3cf6bf331b6eb84c0cc723f06265db7c86bd4e
shbzhang <shbzhang@pmo.ac.cn>
2021-10-27 14:58:30 +0200

    Update cellmap.py
    add __getitem__ method to modify l/b

--- level0/cellmap.py
+++ level0/cellmap.py
@@ -1,4 +1,4 @@
-import glob, re
+import glob, re, os
 import numpy as np
 from math import floor,ceil
 
@@ -9,7 +9,7 @@ class CellMap(np.ndarray):
 	----------
 	patterns : str or list
 		a pathname with '*' or '?' to match files
-	l,b : list, optional
+	l,b : int, float, or list, optional
 		2-elements list that define the map range to show, default shows cells with l=[0,10],b=[-5,5]
 	deluxe : bool, optional
 		format of output, set this keyword true to show a deluxe format.
@@ -21,15 +21,12 @@ class CellMap(np.ndarray):
 		a python version of mapfile.pro, using a subclass of numpy.ndarray
 	Examples
 	--------
-	#check observed cells
-	>>> havecube = CellMap('/share/data/mwisp/G???+00/*.fits',l=[0,20])
+	>>> havecube = CellMap('/share/data/mwisp/G???+00/*.fits')
 	>>> havecube
 
 	#change position
-	>>> havecube.l = [35,50]
-	>>> havecube.b = [-6,6]
 	>>> havecube.deluxe = True
-	>>> havecube
+	>>> havecube[35:50,-6:6]
 	
 	#search of missing info file
 	>>> haveinfo = CellMap('/share/data/mwisp/infofiles/*info.txt')
@@ -66,17 +63,35 @@ class CellMap(np.ndarray):
 	def baxis(self):
 		return np.arange(200,-201,-5)
 
-	@property
-	def lindex(self):
-		l0 = (self.laxis[0] - floor(max(self.l)*2)*5) //5
-		l1 = (self.laxis[0] - ceil(min(self.l)*2)*5) //5
-		return (l0,l1+1)
+	def _range2slice(ran, a0):
+		if ran is None:
+			return slice(None)
+		if isinstance(ran, (int, float)):
+			ran = [ran-5, ran+5]
+		if len(ran) != 2:
+			raise ValueError('l/b should be a list-like object contain 2 elements')
+		if ran[0] is None:
+			if ran[1] is None:
+				return slice(None)
+			else:
+				r1 = (a0 - floor(ran[1]*2)*5) //5
+				return slice(r1, None)
+		else:
+			if ran[1] is None:
+				r0 = (a0 - ceil(ran[0]*2)*5) //5
+				return slice(None, r0+1)
+			else:
+				r0 = (a0 - floor(max(ran)*2)*5) //5
+				r1 = (a0 - ceil(min(ran)*2)*5) //5
+				return slice(r0, r1+1)
 
 	@property
-	def bindex(self):
-		b0 = (self.baxis[0] - floor(max(self.b)*2)*5) //5
-		b1 = (self.baxis[0] - ceil(min(self.b)*2)*5) //5
-		return (b0,b1+1)
+	def lslice(self):
+		return CellMap._range2slice(self.l, self.laxis[0])
+
+	@property
+	def bslice(self):
+		return CellMap._range2slice(self.b, self.baxis[0])
 
 	def _count(self):
 		#search files
@@ -84,20 +99,36 @@ class CellMap(np.ndarray):
 		for pt in self.patterns:
 			files += glob.glob(pt)
 		#find cellname in files
-		self[:] = 0
+		self*=0
 		for file in files:
-			found = re.search(self.reg, file)
+			found = re.search(self.reg, os.path.basename(file))
 			if found is not None:
 				lname,bname = found.group(1,2)
-				lidx = (self.laxis[0]-int(lname))//5
-				bidx = (self.baxis[0]-int(bname))//5
-				self[bidx,lidx] += 1
+				idx = ((self.baxis[0]-int(bname))//5, (self.laxis[0]-int(lname))//5)
+				super().__setitem__(idx, super().__getitem__(idx)+1)
+
+	def _key2lb(key):
+		if isinstance(key, slice):
+			return (key.start, key.stop)
+		if isinstance(key, (int, float)):
+			return key
+		else:
+			return (CellMap._key2lb(k) for k in key[:2])
+
+	def __getitem__(self, key):
+		if isinstance(key, tuple):
+			self.l, self.b = CellMap._key2lb(key)
+		else:
+			self.l = CellMap._key2lb(key)
+		return self
 
 	def __str__(self):
-		lindex, bindex = self.lindex, self.bindex
-		laxis = self.laxis[lindex[0]:lindex[1]]/10
-		baxis = self.baxis[bindex[0]:bindex[1]]/10
-		count = self[bindex[0]:bindex[1], lindex[0]:lindex[1]]
+		lslice, bslice = self.lslice, self.bslice
+		laxis = self.laxis[lslice]/10
+		baxis = self.baxis[bslice]/10
+		if len(laxis) >60:
+			return 'Pleare choose a more narrow l range.'
+		count = super().__getitem__((bslice, lslice)).view(np.ndarray)
 		if self.deluxe:
 			#head
 			hline = '-----' + '+---'*count.shape[1] + '+-----\n'
@@ -140,6 +171,7 @@ class CellMap(np.ndarray):
 			#rear
 			output += ' '*7
 			if laxis[0]%1==0:
+				output += ' '*4
 				for l in laxis[2::4]: output += ' |%-6.1f' % l
 			else:
 				output += ' '*6
@@ -157,24 +189,21 @@ class CellMap(np.ndarray):
 			for idx in np.argwhere(self!=0):
 				cells.append('%04i%+04i' % (self.laxis[idx[1]], self.baxis[idx[0]]))
 		else:
-			lindex, bindex = self.lindex, self.bindex
-			laxis = self.laxis[lindex[0]:lindex[1]]
-			baxis = self.baxis[bindex[0]:bindex[1]]
-			count = self[bindex[0]:bindex[1], lindex[0]:lindex[1]]
+			lslice, bslice = self.lslice, self.bslice
+			laxis = self.laxis[lslice]/10
+			baxis = self.baxis[bslice]/10
+			count = super().__getitem__((bslice, lslice))
 			for idx in np.argwhere(count!=0):
 				cells.append('%04i%+04i' % (laxis[idx[1]], baxis[idx[0]]))
 		return cells
 
-
 if __name__ == '__main__':
 	print('>>check observed cells<<')
-	havecube = CellMap('/share/data/mwisp/G???+00/*.fits',l=[0,20])
+	havecube = CellMap('/share/data/mwisp/G???+00/*.fits')
 	print(havecube)
 
 	print('>>change position<<')
-	havecube.l = [35,50]
-	havecube.b = [-6,6]
-	print(havecube)
+	print(havecube[35:51.5,-6:6])
 	
 	print('>>search for missing info file in region shown<<')
 	haveinfo = CellMap('/share/data/mwisp/infofiles/*info.txt')
